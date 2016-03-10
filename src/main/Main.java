b90922199e2802411739fcf0865c91433a4e3cfb
danielemoro <daniele1.moro@mail.polimi.it>
2016-03-10 16:12:13 +0100

    Ciao

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
 import java.net.MulticastSocket;
+import java.net.SocketTimeoutException;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -37,7 +40,7 @@ public class Main {
 		
 		String[] optionsStart = {"NEW", "JOIN"};
 		int choice = JOptionPane.showOptionDialog(frame,
-				"no pain no gain, dani moro secco!",
+				"",
 				"JOIN OR NEW",
 				JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
@@ -55,19 +58,36 @@ public class Main {
 				"");	
 		
 		MulticastSocket s = null; 
+		List<String> sessionList = new ArrayList<String>();
+		int cont =0;
 		try {
 			String msg = "Hello";
 			s = new MulticastSocket(6971);
 			s.joinGroup(globalLan);
-			DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), group, 6971);
+			DatagramPacket hi = new DatagramPacket(msg.getBytes(), msg.length(), globalLan, 6971);
 			s.send(hi);
 			byte[] buf = new byte[1000];
 			DatagramPacket recv = new DatagramPacket(buf, buf.length);
-			s.receive(recv);
+			s.setSoTimeout(10000);
+			try{
+				while(true){
+					s.receive(recv);
+					//Leggere i dati ricevuti dai vari server
+					sessionList.add( new String(recv.getData()) + recv.getAddress());
+					cont++;
+				}
+			}catch(SocketTimeoutException e2){
+				System.out.println("Exception Raised, Timeout Expired");
+			}
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} 
+		
+		for(String st : sessionList){
+			System.out.println(st);
+		}
+		System.out.println("Numero ricezioni: " + cont);
 		
 		switch(choice){
 		case 0: //NEW--> MASTER!!!
@@ -80,7 +100,7 @@ public class Main {
 					null,
 					"");
 			
-			//Manca il Path dove è presente la presentazione (i file jpg)
+			//Manca il Pathv dove è presente la presentazione (i file jpg)
 			
 			try {
 				user = new User(userName, InetAddress.getLocalHost().getHostAddress(), "0");
@@ -101,5 +121,29 @@ public class Main {
 		}
 		
 	}
-
+	
+	static public String generateIp(List<String> alreadyUsedIp) {
+	    List<Integer> lastElements = new ArrayList<Integer>(); 
+	    for(String el : alreadyUsedIp) {
+	      System.out.println(el);
+	      String[] four = el.split("\\.");
+	      lastElements.add(Integer.parseInt(four[3]));
+	    }
+	    int number; 
+	    boolean alreadyPresent = false;
+	    do{
+	      number = 1+(int)(Math.random()*253);
+	      for(Integer el : lastElements) {
+	        if(number == el) {
+	          alreadyPresent = true;
+	        }
+	      }
+	    } while(alreadyPresent);
+	    for(String el : alreadyUsedIp) {
+	      String[] four = el.split("\\.");
+	      System.out.println("" + four[0] + "." + four[1] + "." + four[2] + "." + (number + 1));
+	      return "" + four[0] + "." + four[1] + "." + four[2] + "." + (number + 1);
+	    }
+	    return null;
+	  }
 }
